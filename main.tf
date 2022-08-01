53f67f24898b6154c65eaafae095ff2f2824ab23
Rodrigo Ferreira <57236853+rerf19@users.noreply.github.com>
2022-08-01 16:09:46 +0100

    Add resources
    gateway + route table + route table association + security group

--- main.tf
+++ main.tf
@@ -1,20 +1,77 @@
-resource "aws_vpc" "mtc_vpc"{
-    cidr_block = "10.123.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true
+#VPC
+resource "aws_vpc" "mtc_vpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-        Name = "dev"
-    }
+  tags = {
+    Name = "dev"
+  }
 }
 
+#Subnet
 resource "aws_subnet" "mtc_public_subnet" {
-    vpc_id = aws_vpc.mtc_vpc.id
-    cidr_block = "10.123.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "eu-west-1a"
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-west-1a"
 
-    tags = {
-        Name = "dev-public"
-    }
+  tags = {
+    Name = "dev-public"
+  }
 }
+
+#Gateway
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+#Route table
+resource "aws_route_table" "mtc_public_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
+
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.mtc_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.mtc_internet_gateway.id
+}
+
+#Route table association
+resource "aws_route_table_association" "mtc_public_assoc" {
+  subnet_id      = aws_subnet.mtc_public_subnet.id
+  route_table_id = aws_route_table.mtc_public_rt.id
+}
+
+#Security group
+resource "aws_security_group" "mtc_sg" {
+  name        = "dev_seg"
+  description = "Dev security group"
+  vpc_id      = aws_vpc.mtc_vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+}
+
+#EMI DataSource
+
